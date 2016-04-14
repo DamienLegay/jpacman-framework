90056cfece48e1f0798c5e5346d1b2bf8d4e4c65
Damien Legay <Legay.Damien@hotmail.com>
2016-04-14 16:41:10 +0200

    Oops, forgot that one.

--- src/test/java/nl/tudelft/jpacman/game/AchievementTest.java
+++ src/test/java/nl/tudelft/jpacman/game/AchievementTest.java
@@ -14,6 +14,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * Test class for Achievements.
+ */
 @SuppressWarnings("checkstyle:linelength")
 public class AchievementTest
 {
